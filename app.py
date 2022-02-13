555ea72a3b73536aa93022812c4743cc5c50c682
mdfnunez <65826102+mdfnunez@users.noreply.github.com>
2022-02-13 16:22:27 -0600

    Update app.py

--- app.py
+++ app.py
@@ -18,45 +18,45 @@ with col1:
     imc0=peso/talla**2
     #round se usa para redondear y quitar decimales
     imc=round(imc0,1)
-
-
-with col2:
-    st.image("fit2.png",None,200)
-    imctxt = '<b style= "text-align:center; font-family:Times; color:#2980B9; font-size: 30px;">Índice de masa corporal</b>'
-    st.markdown(imctxt,unsafe_allow_html=True)
-if imc <= 18.5:
+calcular=st.button("Calcular índice de masa corporal")
+if calcular==True:
     with col2:
-        st.warning(imc)
-    bajopeso='<b style= "text-align:center; font-family:Times; color:#2980B9; font-size: 20px;">Estas en bajo peso</b>'
-    insbap='<b style= "text-align:center; font-family:Times; color:#3E9185; font-size: 18px;">Sigue las siguientes recomendaciones para aumentar de peso</b>'
-    with col2:
-        st.markdown(bajopeso,unsafe_allow_html=True)
-    st.markdown(insbap, unsafe_allow_html=True)
-    st.subheader("https://www.medicalnewstoday.com/articles/es/326685")
-elif imc >= 18.6 and imc <=25:
-    with col2:
-        st.success(imc)
-    normpeso='<b style= "text-align:center; font-family:Times; color:#2980B9; font-size: 20px;">Estas en tu peso ideal, sigue así</b>'
-    with col2:
-        st.markdown(normpeso,unsafe_allow_html=True)
-elif imc >= 25 and imc <=30:
-    with col2:
-        st.warning(imc)
-    normpeso='<b style= "text-align:center; font-family:Times; color:#2980B9; font-size: 20px;">Estas en sobrepeso, es importante que bajes de peso para evitar contraer enfermedades como diabetes mellitus e hipertensión arterial</b>'
-    with col2:
-        st.markdown(normpeso,unsafe_allow_html=True)
-elif imc >= 30 and imc <=35:
-    with col2:
-        st.error(imc)
-    ob1peso='<b style= "text-align:center; font-family:Times; color:#2980B9; font-size: 20px;">Obesidad grado I, el riesgo de padecer enfermedades metabólicas es alto, se recomienda disminuir de peso inmediatamente</b>'
-    with col2:
-        st.markdown(normpeso,unsafe_allow_html=True)
-elif imc >= 35 and imc <=40:
-    with col2:
-        st.error(imc)
-    ob1peso='<b style= "text-align:center; font-family:Times; color:#2980B9; font-size: 20px;">Obesidad grado II, el riesgo de padecer enfermedades metabólicas es muy alto, se recomienda disminuir de peso inmediatamente</b>'
-    with col2:
-        st.markdown(normpeso,unsafe_allow_html=True)
+        st.image("fit2.png",None,200)
+        imctxt = '<b style= "text-align:center; font-family:Times; color:#2980B9; font-size: 30px;">Índice de masa corporal</b>'
+        st.markdown(imctxt,unsafe_allow_html=True)
+    if imc <= 18.5:
+        with col2:
+            st.warning(imc)
+        bajopeso='<b style= "text-align:center; font-family:Times; color:#2980B9; font-size: 20px;">Estas en bajo peso</b>'
+        insbap='<b style= "text-align:center; font-family:Times; color:#3E9185; font-size: 18px;">Sigue las siguientes recomendaciones para aumentar de peso</b>'
+        with col2:
+            st.markdown(bajopeso,unsafe_allow_html=True)
+        st.markdown(insbap, unsafe_allow_html=True)
+        st.subheader("https://www.medicalnewstoday.com/articles/es/326685")
+    elif imc >= 18.6 and imc <=25:
+        with col2:
+            st.success(imc)
+        normpeso='<b style= "text-align:center; font-family:Times; color:#2980B9; font-size: 20px;">Estas en tu peso ideal, sigue así</b>'
+        with col2:
+            st.markdown(normpeso,unsafe_allow_html=True)
+    elif imc >= 25 and imc <=30:
+        with col2:
+            st.warning(imc)
+        normpeso='<b style= "text-align:center; font-family:Times; color:#2980B9; font-size: 20px;">Estas en sobrepeso, es importante que bajes de peso para evitar contraer enfermedades como diabetes mellitus e hipertensión arterial</b>'
+        with col2:
+            st.markdown(normpeso,unsafe_allow_html=True)
+    elif imc >= 30 and imc <=35:
+        with col2:
+            st.error(imc)
+        ob1peso='<b style= "text-align:center; font-family:Times; color:#2980B9; font-size: 20px;">Obesidad grado I, el riesgo de padecer enfermedades metabólicas es alto, se recomienda disminuir de peso inmediatamente</b>'
+        with col2:
+            st.markdown(normpeso,unsafe_allow_html=True)
+    elif imc >= 35 and imc <=40:
+        with col2:
+            st.error(imc)
+        ob1peso='<b style= "text-align:center; font-family:Times; color:#2980B9; font-size: 20px;">Obesidad grado II, el riesgo de padecer enfermedades metabólicas es muy alto, se recomienda disminuir de peso inmediatamente</b>'
+        with col2:
+            st.markdown(normpeso,unsafe_allow_html=True)
 
 #comentar todo lo seleccionado con cmd+shift+/
 # if imc>=18.5 and imc <=24.9:
